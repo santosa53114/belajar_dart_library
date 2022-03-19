8aab13185f128ef0d48cf43026e2e089d11a770f
santosa53114 <santosa53114@gmail.com>
2022-03-19 20:43:37 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main(){
+  print(sayHello('Budi'));
+
+  var costumer = Costumer();
+  var category = Category();
+  var products = Product();
+}
---
+++ lib/hello.dart
@@ -0,0 +1,4 @@
+library belajar_dart_library;
+
+export 'src/say_hello.dart'; // semua di dalam say hello di masukkan
+export 'src/costumer.dart' show Category, Product, Costumer;
